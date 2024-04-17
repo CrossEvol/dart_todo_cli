ff941db0634d11e328b91e1eecaf7b1264baa783
创圣大天使Evol <296662402@qq.com>
2024-04-17 15:57:04 +0800

    Todo model

--- lib/src/common.dart
+++ lib/src/common.dart
@@ -1,1 +1,15 @@
-enum Priority {high, medium, low}
+enum Priority { high, medium, low }
+
+enum Status { pending, completed }
+
+class Todo {
+  String id = '';
+  String title = '';
+  String desc = '';
+  Status status = Status.pending;
+  Priority priority = Priority.medium;
+  DateTime createAt = DateTime.now();
+  DateTime updateAt = DateTime.now();
+
+  Todo(this.id,this.title);
+}
